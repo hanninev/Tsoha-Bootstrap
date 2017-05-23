141ffc833aa5085340522434e277d79eaf82d1c4
Ville <ville.hanninen@helsinki.fi>
2017-05-23 16:26:17 +0300

    Muokattu deploy-tiedostoa siten, että tiedostot menevät ensin Melkinpaasiin ja sen jälkeen Usersiin.

--- deploy.sh
+++ deploy.sh
@@ -5,19 +5,14 @@ DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 
 source $DIR/config/environment.sh
 
-echo "Siirretään tiedostot users-palvelimelle..."
+echo "Siirretään tiedostot Melkinpaasiin..."
 
-# Tämä komento siirtää tiedostot palvelimelta
-rsync -z -r $DIR/app $DIR/assets $DIR/config $DIR/lib $DIR/sql $DIR/vendor $DIR/index.php $DIR/composer.json $USERNAME@users2017.cs.helsinki.fi:htdocs/$PROJECT_FOLDER
+# Siirretään ensin Melkipaasiin ja sitten yhteys Melkkiin
 
-echo "Valmis!"
-
-echo "Suoritetaan komento php composer.phar dump-autoload..."
-
-# Suoritetaan php composer.phar dump-autoload
-ssh $USERNAME@users2017.cs.helsinki.fi "
-cd htdocs/$PROJECT_FOLDER
-php composer.phar dump-autoload
+rsync -avH /home/hanninev/NetBeansProjects/Verkkokauppa/ -e ssh hanninev@melkinpaasi.cs.helsinki.fi:/home/hanninev/Tsoha
+ssh hanninev@melkinpaasi.cs.helsinki.fi "
+cd Tsoha/
+bash deployo.sh
 exit"
 
-echo "Valmis! Sovelluksesi on nyt valmiina osoitteessa $USERNAME.users.cs.helsinki.fi/$PROJECT_FOLDER"
+echo "Valmis! Tiedostot siirretty Melkinpaasiin"
